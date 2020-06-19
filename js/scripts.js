b18a3013c07febd38e0a2e77feeadf5006de0f3e
Megan Hepner <megan.hepner@gmail.com>
2020-06-19 11:46:15 -0700

    create and test price individual prototype functions for size, crust, and sauce

--- js/scripts.js
+++ js/scripts.js
@@ -6,29 +6,51 @@ function Pizza(size, crust, sauce, toppings){
   this.toppings = toppings
 }
 
-Pizza.prototype.sizePrice = function(crust, price){
-  let outputPrice = 0;
-  owner.price = [];
-  console.log(owner.price);
-  if (crust === "chewy" || crust === "thin"){
-    crustPrice = "0";
-  } else {
-    crustPrice = "2";
+Pizza.prototype.sizePrice = function(size){
+  let sizePrice = 0;
+  if (size === "10"){
+    sizePrice = "8";
+  } else if (size === "12") {
+    sizePrice = "10";
+  } else if (size === "16") {
+    sizePrice = "14";   
+  } else if (size === "20") {
+      sizePrice = "15";
+  } else if (size === "22") {
+      sizePrice = "16";
   }
-  console.log(crustPrice);
-  }
+  return(sizePrice);
+}
 
 Pizza.prototype.crustPrice = function(crust){
-  let outputPrice = 0;
+  let crustPrice = 0;
   if (crust === "chewy" || crust === "thin"){
     crustPrice = "0";
   } else {
     crustPrice = "2";
   }
-  console.log(crustPrice);
+  return crustPrice;
+}
+
+Pizza.prototype.saucePrice = function(sauce){
+  let outputPrice = 0;
+  if (sauce === "red" || sauce === "bare"){
+    saucePrice = "0";
+  } else {
+    saucePrice = "2";
   }
+  console.log(saucePrice);
+}
 
-
+Pizza.prototype.saucePrice = function(sauce){
+  let outputPrice = 0;
+  if (sauce === "red" || sauce === "bare"){
+    saucePrice = "0";
+  } else {
+    saucePrice = "2";
+  }
+  console.log(saucePrice);
+}
 
 
   //UI Logic
@@ -42,10 +64,13 @@ $(document).ready(function() {
     $("input:checkbox[name=pizza-toppings]:checked").each(function() {
       const toppings = $(this).val();
       toppings.push(pizza-toppings);
+      const 
     });
     let pizza = new Pizza(size, crust, sauce, toppings);
     const sizePrice = pizza.sizePrice(size);
     const crustPrice = pizza.crustPrice(crust);
+    const saucePrice = pizza.saucePrice(sauce);
+    const toppingsPrice = pizza.toppingsPrice(toppings);
    
   });
 
