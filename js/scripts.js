6fbec3856715b1b34243f8a4a7ffa78bb2b3cbec
Megan Hepner <megan.hepner@gmail.com>
2020-06-19 10:03:37 -0700

    add new HTML page for owner to adjust pricing, successfully connect to js with test

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,50 @@ function Pizza(size, crust, sauce, toppings){
   this.toppings = toppings
 }
 
-Pizza.prototype.pizzaPrice = function(ticketPrice, discountPrice){
+Pizza.prototype.sizePrice = function(crust){
   let outputPrice = 0;
-  outputPrice = ticketPrice - (discountPrice * this.discounts);
-  return outputPrice;
-}
+  if (crust === "chewy" || crust === "thin"){
+    crustPrice = "0";
+  } else {
+    crustPrice = "2";
+  }
+  console.log(crustPrice);
+  }
 
+Pizza.prototype.crustPrice = function(crust){
+  let outputPrice = 0;
+  if (crust === "chewy" || crust === "thin"){
+    crustPrice = "0";
+  } else {
+    crustPrice = "2";
+  }
+  console.log(crustPrice);
+  }
+
+
+
+
+  //UI Logic
 $(document).ready(function() {
   $("form#userInput").submit(function(event){ 
     event.preventDefault();
     const size = $("input:radio[name=pizza-size]:checked").val();
     const crust = $("input:radio[name=pizza-crust]:checked").val();
     const sauce = $("input:radio[name=pizza-sauce]:checked").val();
+    let toppings = [0];
     $("input:checkbox[name=pizza-toppings]:checked").each(function() {
       const toppings = $(this).val();
-      console.log(toppings);
+      toppings.push(pizza-toppings);
     });
+    let pizza = new Pizza(size, crust, sauce, toppings);
+    const sizePrice = pizza.sizePrice(size);
+    const crustPrice = pizza.crustPrice(crust);
+   
+  });
+  $("form#ownerInput").submit(function(event){ 
+    event.preventDefault();
+    const price = $("input:radio[name=pizza-price]:checked").val();
+    console.log(price);
+
   });
 });
