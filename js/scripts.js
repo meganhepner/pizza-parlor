33fb47bda4be0320c24d874aa16f8f7cd8593cc7
Megan Hepner <megan.hepner@gmail.com>
2020-06-19 11:28:49 -0700

    abandon owner project as outside current capability, clean out all owner code

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,10 @@ function Pizza(size, crust, sauce, toppings){
   this.toppings = toppings
 }
 
-function Owner(price){
-  this.price = price
-}
-
- function displayPrice(owner) { 
-  $("#price-10").text(owner.price) ;
-  console.log(price);
-  // $("#player2-score").text(game.player2.score) //score display for player2
-  // $("#turn-total").text(game.turnTotal) //turnTotal display 
-}
-
 Pizza.prototype.sizePrice = function(crust, price){
   let outputPrice = 0;
   owner.price = [];
-  console.log(price);
+  console.log(owner.price);
   if (crust === "chewy" || crust === "thin"){
     crustPrice = "0";
   } else {
@@ -44,13 +33,6 @@ Pizza.prototype.crustPrice = function(crust){
 
   //UI Logic
 $(document).ready(function() {
-  $("form#ownerInput").submit(function(event){ 
-    event.preventDefault();
-    const price = $("input:radio[name=pizza-price]:checked").val();
-    let owner = new Owner(price);
-    displayPrice(owner);
-
-  });
   $("form#userInput").submit(function(event){ 
     event.preventDefault();
     const size = $("input:radio[name=pizza-size]:checked").val();
@@ -62,7 +44,6 @@ $(document).ready(function() {
       toppings.push(pizza-toppings);
     });
     let pizza = new Pizza(size, crust, sauce, toppings);
-    let price = displayPrice(owner);
     const sizePrice = pizza.sizePrice(size);
     const crustPrice = pizza.crustPrice(crust);
    
