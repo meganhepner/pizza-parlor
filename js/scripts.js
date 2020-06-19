e484b3bdecc846d45bebabf90689b1708492b02a
Megan Hepner <megan.hepner@gmail.com>
2020-06-19 09:02:47 -0700

    add HTML text for checkbox form including size crust sauce toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+//Business Logic
+function Pizza(size, crust, sauce, toppings){
+  this.size = size,
+  this.crust = crust,
+  this.sauce = sauce,
+  this.toppings = toppings
+}
+
+Pizza.prototype.pizzaPrice = function(ticketPrice, discountPrice){
+  let outputPrice = 0;
+  outputPrice = ticketPrice - (discountPrice * this.discounts);
+  return outputPrice;
+}
+
 $(document).ready(function() {
-  
+  $("form#userInput").submit(function(event){ 
+    event.preventDefault();
+  });
 });
